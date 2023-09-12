8cbf5e40d981f08915f2cf76ccda7d1c881acb8b
Fawaz Oduola <oduolafawaz@gmail.com>
2023-09-12 23:01:00 +0100

    added field validation

--- controller/personController.js
+++ controller/personController.js
@@ -15,11 +15,14 @@ class personController{
             }
         }
 
+        if (typeof(data["name"])!=String){
+            res.status(400).json("use only string type for name")
+        }
         
         try{
             const createdPerson = await personSchema.create(data);
 
-            res.json(`user ${data.name} created`)
+            res.status(201).json(`user ${data.name} created`)
         }catch(error){
             res.json(`error creating user`)
         }        
@@ -27,7 +30,7 @@ class personController{
     
     async  getAllPersons(req, res){
         const persons = await personSchema.getAll();
-        res.json(persons)
+        res.status(200).json(persons)
     }
 
     async getPersonById(req, res){
@@ -36,9 +39,9 @@ class personController{
         if (id){
             try {
                 const person = await personSchema.getById(id);
-                res.json(person)
+                res.status(200).json(person)
             } catch (error) {
-                res.json("error getting user")
+                res.status(404).json("user not found")
             }
         }
     }
@@ -49,9 +52,9 @@ class personController{
         if (id){
             try {
                 const deletedPerson = await personSchema.delete(id);
-                res.json(`person with id ${deletedPerson.id} successfully deleted`)
+                res.status(204).json(`person with id ${deletedPerson.id} successfully deleted`)
             } catch (error) {
-                res.json("error deleting")
+                res.status(404).json("error deleting")
             }
         }
     }
@@ -62,9 +65,9 @@ class personController{
         if (id && data){
             try {
                 const updatedPerson = await personSchema.update(id, data);
-                res.send(`person with ${updatedPerson.id} successfully updated`)
+                res.status(200).send(`person with ${updatedPerson.id} successfully updated`)
             } catch (error) {
-                res.send("error updating")
+                res.status(404).send("error updating")
             }
         }
     }
